527aa62788b518e3627eee8cecd9844c8caf62f6
Neel Mehta <neelmehta@college.harvard.edu>
2017-07-08 13:30:53 -0400

    move over copyright

--- propublica.py
+++ propublica.py
@@ -11,10 +11,6 @@ manualdata.csv (any data that is only available via pdf should be filled
 Output:
 finaldata.csv (data autograbbed from the API merged with manual data)
 
-
-
-By the Harvard Open Data Project, copyright (c) 2017
-
 """
 
 import json
